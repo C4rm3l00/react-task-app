f369d4c213f49024f7a902ce76baeda591e8b6ac
carmelo <gabrielnaut1@gmail.com>
2024-10-20 19:46:38 -0400

    vercel analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Tasklist from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
+      <Analytics/>
       <TaskForm />
       <Tasklist />
     </>
